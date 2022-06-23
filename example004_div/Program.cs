41a8ea60f31098c1ce1433cb6624b6817d5fd63d
Hejis <simpaa@inbox.ru>
2022-06-23 23:26:10 +0400

    Example005randomПотеряли divv

--- example004_div/Program.cs
+++ example004_div/Program.cs
@@ -1,3 +1,6 @@
-﻿int numberA = 12;
-int numberB = 5;
-Console.WriteLine(numberA / numberB);
+﻿int numberA =  new Random(1,10); // 1 2 3 4 ...9
+Console.WriteLine(numberA)
+int numberB = new NonRandom().Next(1,10);
+Console.WriteLine(muberB);
+Console.WriteLine(numberA + numberB);
+Console.WriteLine(result);
